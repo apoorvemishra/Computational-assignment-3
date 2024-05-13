913f9a9be56b0e4c7b39887d73010d517768d54c
apoorvemishra <159141272+apoorvemishra@users.noreply.github.com>
2024-05-13 20:54:52 +0530

    Add files via upload

---
+++ Question12.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def g(x):
+    return np.exp(-x*x)
+def h(x):
+    return np.exp(-4*x*x)
+def conv(x):
+    return np.exp(-4*x*x/5)*np.sqrt(np.pi/5)
+
+
+n = 20
+x = np.linspace(-n/2, n/2, 1000)
+y_g = g(x)
+y_h = h(x)
+y_fft = np.fft.fft(y_g, norm = 'ortho')*np.fft.fft(y_h, norm = 'ortho')
+y_num = np.fft.fft(y_fft, norm='ortho')
+y_dft = np.append(y_num[int(len(x)/2)+1:], y_num[:int(len(x)/2)+1])
+y_dft = y_dft*np.sqrt(n*(x[1]-x[0]))
+y = conv(x)
+
+
+
+
+plt.plot(x, y, label ='analytical convolution')
+plt.plot(x, np.abs(y_dft), label = 'Convolution using DFT')
+plt.title('Convolved Function')
+plt.xlabel('x')
+plt.ylabel('[g*h](x)')
+
+
+plt.tight_layout()
+plt.legend()
+plt.show()
